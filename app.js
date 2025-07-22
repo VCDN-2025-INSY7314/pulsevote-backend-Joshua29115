578f04ceff62be1029e2f54c0fd08e776d154ca6
Joshua Pillay <jrpillay7@gmail.com>
2025-07-22 11:18:56 +0200

    Initial backend setup

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+
+//app.use(helmet());
+app.use(cors({
+  origin: 'http://localhost:5173', // Match your frontend's dev server URL
+  optionsSuccessStatus: 200 // Some browsers require this for older APIs
+}));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('PulseVote API running!');
+});
+
+module.exports = app;
