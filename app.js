6542c13307c5e6cc1227c08143d7cd3647c9d7db
Joshua Pillay <jrpillay7@gmail.com>
2025-07-30 14:38:22 +0200

    Completed JWT auth integration and testing

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 const dotenv = require('dotenv');
+const authRoutes = require("./routes/authRoutes");
+const { protect } = require("./middleware/authMiddleware");
 
 dotenv.config();
 
@@ -9,10 +11,15 @@ const app = express();
 
 //app.use(helmet());
 app.use(cors({
-  origin: 'http://localhost:5173', // Match your frontend's dev server URL
+  origin: 'https://localhost:5173', // Match your frontend's dev server URL
   optionsSuccessStatus: 200 // Some browsers require this for older APIs
 }));
 app.use(express.json());
+
+app.use(cors({
+  origin: "https://localhost:5173",
+  credentials: true
+}));
 
 app.get('/', (req, res) => {
   res.send('PulseVote API running!');
@@ -23,3 +30,12 @@ module.exports = app;
 app.get('/test', (req, res) => {
   res.json({ message: 'Test JSON from backend' });
 });
+
+app.get("/api/protected", protect, (req, res) => {
+  res.json({
+    message: `Welcome, user ${req.user.id}! You have accessed protected data.`,
+    timestamp: new Date()
+  });
+});
+
+app.use("/api/auth", authRoutes);
--- server.js
+++ server.js
@@ -4,13 +4,20 @@ const https = require('https');
 const fs = require('fs');
 require('dotenv').config();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 const options = {
   key: fs.readFileSync('ssl/key.pem'),
   cert: fs.readFileSync('ssl/cert.pem'),
 };
 
-https.createServer(options, app).listen(PORT, () => {
-  console.log('Server running at https://localhost:' + PORT);
+console.log(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+    https.createServer(options, app).listen(PORT, () => {
+    console.log('Server running at https://localhost:' + PORT);
+    });
+})
+.catch((err) => {
+    console.error('MongoDB connection error:', err);
 });
